dc52310d96a8e88a9783b2fbe18514f5d9d6853e
smart-onion <154732603+smart-onion@users.noreply.github.com>
2025-09-29 18:21:04 +0300

    header

---
+++ src/components/CustomHeader.tsx
@@ -0,0 +1,39 @@
+import {Header} from "antd/es/layout/layout";
+import {Flex, Menu} from "antd";
+import {useNavigate} from "react-router";
+import {useAppDispatch, useAppSelector} from "../app/hooks.ts";
+import {logout, selectUser} from "../features/userSlice.ts";
+import MenuItem from "antd/es/menu/MenuItem";
+
+
+export function CustomHeader() {
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+    const user = useAppSelector(selectUser);
+    const onLogout = () => {
+        dispatch(logout());
+        navigate("/login");
+    }
+
+    return (
+        <>
+            <Header>
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["home"]}>
+                    <Flex vertical={false}  justify="space-between">
+                    {user.isAuthenticated &&
+                        <>
+                            <MenuItem key="home">
+                                Home
+                            </MenuItem>
+
+                            <MenuItem key="logout" onClick={() => onLogout()}>
+                                Logout
+                            </MenuItem>
+                        </>
+                    }
+                    </Flex>
+                </Menu>
+            </Header>
+        </>
+    )
+}
